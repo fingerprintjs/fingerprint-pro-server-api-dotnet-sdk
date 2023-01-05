471c5bc53b1a91a6d8ae7e4c2f489b2959bf1528
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-05 13:10:25 +0100

    chore: fix broken client

--- generate.sh
+++ generate.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-VERSION='1.2.0'
-
-while getopts "v:" arg; do
-  case $arg in
-    v)
-      VERSION=$OPTARG
-      ;;
-  esac
-done
-
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
 java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l csharp -i ./res/fingerprint-server-api.yaml -o ./ -c config.json -DpackageVersion=$VERSION
