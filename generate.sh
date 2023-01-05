f4268c58cb2dc00721d2dd849a9579531adbc8c9
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-05 10:19:45 +0100

    chore: initial commit

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION='1.2.0'
+
+while getopts "v:" arg; do
+  case $arg in
+    v)
+      VERSION=$OPTARG
+      ;;
+  esac
+done
+
+# jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
+
+java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l csharp-dotnet2 -i ./res/fingerprint-server-api.yaml -o ./ -c config.json -DpackageVersion=$VERSION
