e3fb3a55388f7416e77b7797e0d02532077b5455
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-11 11:47:32 +0100

    refactor: cleanup templates

--- src/sdk/Client/ExceptionFactory.cs
+++ src/sdk/Client/ExceptionFactory.cs
@@ -10,12 +10,13 @@
 
 using RestSharp;
 
-namespace sdk.Client;
-
-/// <summary>
-///     A delegate to ExceptionFactory method
-/// </summary>
-/// <param name="methodName">Method name</param>
-/// <param name="response">Response</param>
-/// <returns>Exceptions</returns>
-public delegate Exception ExceptionFactory(string methodName, IRestResponse response);
+namespace sdk.Client
+{
+    /// <summary>
+    /// A delegate to ExceptionFactory method
+    /// </summary>
+    /// <param name="methodName">Method name</param>
+    /// <param name="response">Response</param>
+    /// <returns>Exceptions</returns>
+    public delegate Exception? ExceptionFactory(string methodName, IRestResponse response);
+}
