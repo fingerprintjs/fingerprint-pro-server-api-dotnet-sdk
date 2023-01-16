746799076cde5a0f2fb0f56596f0955dbb5668f0
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-16 12:37:19 +0100

    build: bump csproj version as well

--- semantic-release/bumpConfigAndGenerate.mjs
+++ semantic-release/bumpConfigAndGenerate.mjs
@@ -5,18 +5,53 @@ import cp from 'child_process'
 
 const dirname = import.meta.url.replace(/^file:\/\//, '');
 
-const version = process.env.NEW_VERSION;
+function getVersion() {
+    const version = process.env.NEW_VERSION;
 
-if (!version) {
-    throw new Error('NEW_VERSION environment variable is not set');
+    if (!version) {
+        throw new Error('NEW_VERSION environment variable is not set');
+    }
+
+    console.info('New version:', version);
+
+    return version;
 }
 
-console.info('New version:', version);
+function bumpConfigVersion(version) {
+    const configPath = path.resolve(dirname, '../../config.json');
 
-config.packageVersion = version;
+    console.info('Config path:', configPath);
 
-fs.writeFileSync(path.resolve(dirname, '../config.json'), JSON.stringify(config, null, 4));
+    config.packageVersion = version;
 
-cp.execSync(`sh ${path.resolve(dirname, '../generate.sh')}`, {
-    stdio: 'inherit'
-});
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 4));
+}
+
+function bumpCsprojVersion(version) {
+    const csprojPath = path.resolve(dirname, '../../src/Fingerprint.Sdk/Fingerprint.Sdk.csproj');
+
+    console.info('Csproj path:', csprojPath);
+
+    const csproj = fs.readFileSync(csprojPath, 'utf8');
+
+    // Replace <Version> tag with given version
+    const newCsproj = csproj.replace(/<Version>.*<\/Version>/, `<Version>${version}</Version>`);
+
+    fs.writeFileSync(csprojPath, newCsproj);
+}
+
+function generateSwaggerCode() {
+    cp.execSync(`sh ${path.resolve(dirname, '../../generate.sh')}`, {
+        stdio: 'inherit'
+    });
+}
+
+function main() {
+    const version = getVersion();
+
+    bumpConfigVersion(version);
+    bumpCsprojVersion(version);
+    generateSwaggerCode();
+}
+
+main();
