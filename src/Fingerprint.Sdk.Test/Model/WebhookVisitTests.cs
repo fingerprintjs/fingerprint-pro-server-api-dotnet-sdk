12b666fbabd6456da3b124f7b392a1a41dbfad34
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-16 12:56:49 +0100

    test: add webhook test

---
+++ src/Fingerprint.Sdk.Test/Model/WebhookVisitTests.cs
@@ -0,0 +1,20 @@
+using System.Text;
+using Fingerprint.Sdk.Model;
+using Fingerprint.Sdk.Test.Utils;
+using Newtonsoft.Json;
+
+namespace Fingerprint.Sdk.Test.Model;
+
+[TestFixture]
+public class WebhookVisitTests
+{
+    [Test]
+    public void FromBytesTest()
+    {
+        var bytes = MockLoader.Load("webhook.json");
+        var json = Encoding.UTF8.GetString(bytes);
+        var webhook = JsonConvert.DeserializeObject<WebhookVisit>(json);
+        
+        Assert.That(webhook, Is.InstanceOf<WebhookVisit>());
+    }
+}
