80169403bf87d6310d84c8ef3a87bffdb43b0716
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-01-17 12:23:53 +0100

    build: fix invalid nuget package name

--- semantic-release/publish.mjs
+++ semantic-release/publish.mjs
@@ -45,7 +45,7 @@ cp.execSync('dotnet pack --configuration Release --no-restore', {
     cwd: paths.sdk
 })
 
-const fileName = `Fingerprint.Sdk.${version}.nupkg`;
+const fileName = `FingerprintPro.ServerApi.${version}.nupkg`;
 
 console.info(`Publishing ${fileName}...`);
 
