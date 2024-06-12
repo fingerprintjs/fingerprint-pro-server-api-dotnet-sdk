7b4a7fb952af3837ed82fc4adf675dc9a20a7e29
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-06-12 13:46:21 +0200

    feat: add `Webhook.IsValidWebhookSignature` function for validating webhook signature

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 # clean models and docs before generating
 shopt -s extglob
 rm -f ./src/FingerprintPro.ServerSdk/Model/!(*ASN).cs
-find ./docs -type f ! -name "DecryptionKey.md" ! -name "Sealed.md" -exec rm {} +
+find ./docs -type f ! -name "DecryptionKey.md" ! -name "Sealed.md" ! -name -name "Webhook.md" -exec rm {} +
 
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
