8fc26915b9da212931a54708273c520b922ac4aa
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-11-07 10:46:49 +0100

    chore: add more examples

--- src/FingerprintPro.ServerSdk.Examples/Program.cs
+++ src/FingerprintPro.ServerSdk.Examples/Program.cs
@@ -3,30 +3,75 @@
 using System;
 using FingerprintPro.ServerSdk.Api;
 using FingerprintPro.ServerSdk.Client;
+using FingerprintPro.ServerSdk.Model;
 
 
-var configuration = new Configuration(Environment.GetEnvironmentVariable("API_KEY")!);
+var example = new FingerprintExample();
+example.GetVisitsExample();
+example.GetEventExample();
 
-// Change region if needed
-var region = Environment.GetEnvironmentVariable("REGION")!;
-if (region == "eu")
+// Uncomment following line to test update event method
+// example.UpdateEventExample()
+
+// Uncomment following line to test delete visitor data
+// example.DeleteVisitorDataExample()
+
+internal class FingerprintExample
 {
-    configuration.Region = Region.Eu;
+    private readonly Configuration _configuration = new(Environment.GetEnvironmentVariable("API_KEY")!);
+    private readonly string _visitorId = Environment.GetEnvironmentVariable("VISITOR_ID")!;
+    private readonly string _requestId = Environment.GetEnvironmentVariable("REQUEST_ID")!;
+    private readonly FingerprintApi _api;
+
+    public FingerprintExample()
+    {
+        // Change region if needed
+        var region = Environment.GetEnvironmentVariable("REGION")!;
+        _configuration.Region = region switch
+        {
+            "eu" => Region.Eu,
+            "ap" => Region.Asia,
+            _ => _configuration.Region
+        };
+
+        _api = new FingerprintApi(_configuration);
+    }
+
+
+    public void GetVisitsExample()
+    {
+        var visits = _api.GetVisits(_visitorId);
+        Console.WriteLine("GetVisits() result:");
+        Console.WriteLine(visits);
+    }
+
+    public void GetEventExample()
+    {
+        var events = _api.GetEvent(_requestId);
+        Console.WriteLine("GetEvent() result:");
+        Console.WriteLine(events);
+    }
+
+    public void UpdateEventExample()
+    {
+        var tag = new Tag
+        {
+            ["sdk"] = "dotnet"
+        };
+
+        var body = new EventsUpdateRequest()
+        {
+            Suspect = false,
+            Tag = tag,
+            LinkedId = "<linked_id>"
+        };
+        _api.UpdateEvent(body, _requestId);
+        Console.WriteLine("Event updated");
+    }
+
+    public void DeleteVisitorDataExample()
+    {
+        _api.DeleteVisitorData(_visitorId);
+        Console.WriteLine("Scheduled visitor data removal");
+    }
 }
-else if (region == "ap")
-{
-    configuration.Region = Region.Asia;
-}
-
-var api = new FingerprintApi(
-    configuration
-);
-
-var requestId = Environment.GetEnvironmentVariable("REQUEST_ID")!;
-var visitorId = Environment.GetEnvironmentVariable("VISITOR_ID")!;
-
-var visits = api.GetVisits(visitorId);
-var events = api.GetEvent(requestId);
-
-Console.WriteLine(visits.ToJson());
-Console.WriteLine(events.ToJson());
