a45435543daf8ce72a0cde6d64922658f2ef0838
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-11-18 10:42:12 +0100

    build: fix docs removal command

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 # clean models and docs before generating
 find ./src/FingerprintPro.ServerSdk/Model -type f ! -name "DictionaryModel.cs" -exec rm {} +
-find ./docs -type f ! -name "DecryptionKey.md" ! -name "Sealed.md" ! -name -name "Webhook.md" -exec rm {} +
+find ./docs -type f ! -name "DecryptionKey.md" ! -name "Sealed.md" !  -name "Webhook.md" -exec rm {} +
 
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
