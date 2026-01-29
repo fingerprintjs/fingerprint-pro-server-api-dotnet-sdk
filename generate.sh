f0910bb853121c74940f8f26a4acefa3f414b56e
Eray Aydın <eray.aydin@fingerprint.com>
2026-01-29 18:45:32 +0400

    build: switch openapi-generator from JAR to Docker
    Replace JAR-based openapi-generator execution with Docker container.
    
    Related-Task: INTER-1491

--- generate.sh
+++ generate.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
+set -euo pipefail
 
 # Cleanup
 rm -Rf ./docs
 rm -Rf ./src/Fingerprint.ServerSdk
 
-# Prepare OpenAPI Generator CLI
-OPENAPI_GENERATOR_FILE="./bin/openapi-generator-cli.jar"
-OPENAPI_GENERATOR_URL="https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.19.0/openapi-generator-cli-7.19.0.jar"
+OPENAPI_GENERATOR_IMAGE_VERSION="v7.19.0"
 
-if [[ ! -e "$OPENAPI_GENERATOR_FILE" ]]; then
-  echo "Downloading $OPENAPI_GENERATOR_URL..."
-  mkdir -p "$(dirname "$OPENAPI_GENERATOR_FILE")"
-  curl -fSL -o "$OPENAPI_GENERATOR_FILE" "$OPENAPI_GENERATOR_URL"
-fi
-
-# Generate
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-java -Duser.language=en -Duser.country=US -Duser.variant="" \
-  -jar "$OPENAPI_GENERATOR_FILE" generate \
-  -t ./template -g csharp \
-  -i ./res/fingerprint-server-api.yaml -o ./ -c config.json
+docker run --rm -v "${PWD}:/local" -w /local "openapitools/openapi-generator-cli:${OPENAPI_GENERATOR_IMAGE_VERSION}" generate \
+  -i ./res/fingerprint-server-api.yaml \
+  -g csharp \
+  -o ./ \
+  -t ./template \
+  -c ./config.json
 
 # Fix api doc
 mv ./docs/apis/FingerprintApi.md ./docs/FingerprintApi.md
